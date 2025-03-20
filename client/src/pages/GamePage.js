63a5b60af0401cf6fcb0ee4c9b5dadb254f83144
LorenzoCag <lorenzocagliero@princeton.edu>
2025-03-20 12:24:53 -0400

    everything is hardcoded

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './GamePage.css';
 
+// everything hardcoded
 const GamePage = () => {
     const { levelId } = useParams();
     const navigate = useNavigate();
