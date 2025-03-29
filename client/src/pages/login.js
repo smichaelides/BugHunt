83d641fe80f53a98af1f345e086be6ebcd0a7c22
Sofia Michaelides <smichaelides@princeton.edu>
2025-03-28 20:28:58 -0400

    changed spelling

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useCallback } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 
 const Login = () => {
   const { 
