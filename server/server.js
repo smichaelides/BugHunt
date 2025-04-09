a5878b76969286c7a0b3d13ed13be2f7fb7179f2
LorenzoCag <lorenzocagliero@princeton.edu>
2025-04-09 15:14:13 -0400

    Increase leaderboard limit from 5 to 10 users

--- server/server.js
+++ server/server.js
@@ -209,7 +209,7 @@ app.get('/api/leaderboard', async (req, res) => {
             FROM public.users 
             WHERE points > 0  /* Only show users with points */
             ORDER BY points DESC 
-            LIMIT 5
+            LIMIT 3
         `;
         
         console.log('Executing query:', query);
